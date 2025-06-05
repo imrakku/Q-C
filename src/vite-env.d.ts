38dccf5264d5c0bac87bfba11f85b75f00830303
imrakku <rakshitarora123@gmail.com>
2025-06-06 02:41:14 +0530

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+// If you are using Leaflet, Chart.js etc. globally from CDN:
+// You might want to declare them on the window object if TypeScript complains
+// For example:
+// declare global {
+//   interface Window {
+//     L: typeof import('leaflet');
+//     Chart: typeof import('chart.js').Chart;
+//     d3: typeof import('d3');
+//     HeatmapOverlay: any; // Or find a more specific type if available
+//     h337: any; // For heatmap.js core
+//   }
+// }
